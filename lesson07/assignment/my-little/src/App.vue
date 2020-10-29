3985061efa28331d3a97b680524d738b1f236cfd
swati <swatidash2007@gmail.com>
2020-10-28 19:03:28 -0700

    some file changes

--- lesson07/assignment/my-little/src/App.vue
+++ lesson07/assignment/my-little/src/App.vue
@@ -2,17 +2,6 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view/>
-    <div class="nav-bar navbar-expand">
-      <nav>
-        <ul class="nav-links">
-          <li class="link"><a href="index.html">Home</a></li>
-          <li class="link"><a href="about.html">About Us</a></li>
-          <li class="link"><a href="yoga.html">Yoga</a></li>
-          <li class="link"><a href="pranayama.html">Pranayama</a></li>
-
-        </ul>
-      </nav>
-    </div>
   </div>
 </template>
 
@@ -23,6 +12,7 @@ export default {
 </script>
 
 <style>
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- lesson07/assignment/my-little/src/main.js
+++ lesson07/assignment/my-little/src/main.js
@@ -1,11 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
 import router from './router'
-import 'bootstrap'
+
 import BootstrapVue from 'bootstrap-vue'
-import './main.scss'
+//import './main.scss'
+
+Vue.use(BootstrapVue)
+
+
+
 
 Vue.config.productionTip = false
 
