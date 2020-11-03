86da1ba537c14f40bcde938e9cad95f9df7f643c
swati <swatidash2007@gmail.com>
2020-11-03 00:45:29 -0800

    added nav, sass

--- lesson07/assignment/my-yoga-app/src/components/Home.vue
+++ lesson07/assignment/my-yoga-app/src/components/Home.vue
@@ -5,7 +5,7 @@
 <div class="header">
      <header>
           <div class="logo">
-           <img src="/image/yoga-chakra.jpg" id ="yoga-chakra" alt="man sitting in a yogic posture"/>
+           <img src="image/yoga-chakra.jpg" id ="yoga-chakra" alt="man sitting in a yogic posture"/>
 
           </div>
          <h1>Yoga</h1>
@@ -15,10 +15,68 @@
      <div class="nav-bar navbar-expand">
        <nav>
          <ul class="nav-links">
-           <li v-for="item in items"><a href="#">{{item}}</a></li>
+           <li v-for="item in items" v-bind:key="item"><a href="#">{{item}}</a></li>
         </ul>
        </nav>
      </div>
+    <div class="content">
+    <div class="sidebar">
+         <h2>Yoga Basics</h2>
+         <p>
+           Yoga is a vast collection of spiritual techniques and practices aimed at integrating mind, body and spirit to achieve a state of enlightenment or oneness
+           with the universe. What is normally thought of as “yoga” in the West is really Hatha Yoga, one of the many paths of yoga. The different paths of yoga
+           emphasize different approaches and techniques, but ultimately lead to the same goal of unification and enlightenment.Though yoga’s ultimate aim is lofty,
+           its essence is practical and scientific as it emphasizes direct experience and observable results.  It is not a religion, but a practice of personal
+           inquiry and exploration.  As the cultural and religious diversity of practitioners attest, yogic philosophy speaks to universal truths that can be
+           incorporated within any belief system.
+         </p>
+       </div>
+       <main class="main">
+            <div class="card-container">
+              <div class="city">
+                <a href="#">
+                  <div class="city-image">
+                    <img src="image/yoga.jpg" alt="yoga"/>
+                  </div>
+                  <div class="description">
+                    <h2>Karma Yoga</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus, ipsam deleniti excepturi asperiores sit libero natus, porro, unde voluptates similique vel odio! Cum voluptatum optio dolore nobis dicta sapiente, inventore?</p>
+                  </div>
+                </a>
+              </div>
+              <div class="city">
+                <a href="#">
+                  <div class="city-image">
+                    <img src="image/yoga.jpg" alt="yoga"/>
+                  </div>
+                  <div class="description">
+                    <h2>Jnana Yoga</h2>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis cum cumque facere eius rem? Voluptas dolorum voluptate, a ipsam fugiat beatae. Doloremque earum, commodi, accusamus assumenda culpa quam? Cumque, eaque.
+                    </p>
+                  </div>
+                </a>
+              </div>
+              <div class="city">
+                <a href="#">
+                  <div class="city-image">
+                    <img src="image/yoga.jpg" alt="yoga"/>
+                  </div>
+                  <div class="description">
+                    <h2>Hatha Yoga</h2>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate quae praesentium facere ea minus id debitis veritatis aliquid ipsum aut, in iusto hic modi, saepe. Optio asperiores saepe sit natus.
+                    </p>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </main>
+    </div>
+    <div class="footer">
+        <p class="footer-text">© Copyright 2020 Swati. All rights reserved.</p>
+
+      </div>
 </div>
 </div>
 </template>
@@ -29,76 +87,13 @@ export default {
   data() {
   return {
   items: ["Home", "About", "Yoga", "Pranayama"]
+
   }
   }
 
   }
 </script>
 <style>
-html {
-  box-sizing: border-box;
-}
 
-*,
-*:after,
-*:before {
-  box-sizing: inherit;
-}
-.grid-container {
-  display: grid;
-  grid-template-columns: 1fr;
-
-}
-.header {
-  background-color: rgb(183, 110, 121);
-  height: 20rem;
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  }
-  #yoga-chakra {
-  height: 8rem;
-  width: 8rem;
-  border-radius: 50%;
- }
-
-  h1 {
-
-    display: flex;
-    justify-content: center;
-    color: white;
-  }
-  .nav-bar {
-    background-color: rgb(31, 73, 227);
-  }
-  nav {
-
-  height: 10vh;
-  background: rgb(179, 83, 137);
-  }
-  .nav-links {
-    display: flex;
-    list-style: none;
-    width: 100%;
-    justify-content: space-around;
-    align-items: center;
-    height: 100%;
-    margin-left: auto;
-
-  }
-  li a {
-    color: white;
-    text-decoration: none;
-    font-size: 1.3rem;
-    display: flex;
-  }
-  li a:hover {
-   text-decoration: none;
-   border-top: 4px solid white;
-   border-bottom: 4px solid white;
-   padding: 6px 0;
-   color:$sidebar-heading;
- }
 
 </style>
