ea4cfd0e116fbb8f29955c8a0d1d7a893e09fd2e
swati <swatidash2007@gmail.com>
2020-11-19 15:47:16 -0800

    creating card component

---
+++ lesson08/assignment/my-yoga-app/src/components/Card.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+name: 'card',
+props: ['card'],
+
+}
+</script>
+
+<template>
+
+<div class="card-container">
+<div class="city" v-for="card in cards" v-bind:key="card">
+<a href="#">
+<div class="city-image">
+<img v-bind:src="card.img" />
+</div>
+<div class="description">
+<h2>{{card.title}}</h2>
+<p>{{card.desc}}</p>
+</div>
+</a>
+</div>
+</div>
+
+</template>
